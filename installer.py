110c0208be1d1c18678e4899ff7d6dd4a0e2eecf
dsx12 <45248846+dsx12@users.noreply.github.com>
2020-04-27 20:28:11 -0400

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,21 +1,23 @@
 import os
 
 if __name__ == '__main__':
+	commands = ['sudo sudo protonvpn init', 'systemctl enable protonvpn.service',
+	'systemctl start protonvpn.service']
 	if os.getuid() != 0:
 		print('you have to run this script with sudo')
 		exit(1)
-
 	try:
 		with open('service.txt', 'r') as f:
 			contents = f.read()
 		with open('/etc/systemd/system/protonvpn.service', 'w') as f:
 			f.write(contents)
 	except FileNotFoundError as e:
-		print(e)
-		print('Make sure the service.txt is in the same directory of install.py!')
+		print('Make sure the service.txt is in the same directory of install.py and the systemd timer path is correct in the script! Error: ', e)
 		exit(1)
-	
+		
 	print('running proton vpn init, enter your configuration settings..')
-	os.system('sudo sudo protonvpn init')
-	os.system('systemctl enable protonvpn.service')
-	os.system('systemctl start protonvpn.service')
+	for command in commands:
+		os.system(command)
+	
+	print('ProtonVPN installed, now at reboot ProtonVPN will automatically connect')
+	print('To check status of protonvpn enter sudo systemctl status protonvpn.service')
